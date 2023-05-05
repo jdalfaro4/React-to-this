9aefc07b6acd3809581e710ff50cda806a09d50d
jdalfaro4 <jdalfaro4@gmail.com>
2023-05-04 20:51:28 -0500

    create contactme file, about me and resume files

---
+++ react-to-this/src/components/Contactme.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function Contact() {
+    return (
+        <div className="container-fluid bg-info" style={{ minHeight: '100vh' }}>
+            <div className="row">
+                <div className="col-md-6 offset-md-1">
+                    <h2 >Contact Me:</h2>
+                    <form>
+                        <div className="form-group">
+                            <label htmlFor="name" >Name:</label>
+                            <input type="text" className="form-control" id="name" name="name" placeholder="Enter your name" required />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email" >Email address:</label>
+                            <input type="email" className="form-control" id="email" name="email" placeholder="Enter your email" required />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="message" >Message:</label>
+                            <textarea className="form-control" id="message" name="message" rows="5" placeholder="Enter your message" required ></textarea>
+                        </div>
+                        <button type="submit" className="btn btn-dark text-white" >Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Contact;
--- react-to-this/src/components/Navigation.js
+++ react-to-this/src/components/Navigation.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Footer from './Footer';
 import Header from './Header';
 // import Project from './Project';
+import '../App.css';
 
 function Navigation() {
     return (
@@ -9,14 +10,11 @@ function Navigation() {
             <Header />
             <div className="container-fluid bg-info py-5">
                 <div className="row">
-                    <div className="col-md-6 offset-md-1">
-                        <h2>About Me</h2>
-                        <p class="aboutmeparagraph">I'm energetic, outgoing, creative, and hardworking! I love the world of coding and can't get enough of learning everyday.
-                            I have a beauitful wife who makes my day just by waking up in the morning and I have three crazy kids who are my world. 
-                            The more I learn, the more I love life, so you'll almost always find me with a book in my hand. "Take care of yourself as if you
-                            were someone you were responsible for"
+                    <section class="welcome" className="col-md-6 offset-md-1">
+                        <h2>Welcome!</h2>
+                        <p>Welcome to my page!
                         </p>
-                    </div>
+                    </section>
                 </div>
             </div>
             {/* <Project /> */}

febd560584f9432e846846b82cfabc60bd3fca03
jdalfaro4 <jdalfaro4@gmail.com>
2023-05-08 21:54:01 -0500

    add css to contact me tab

--- react-to-this/src/components/Contactme.js
+++ react-to-this/src/components/Contactme.js
@@ -6,7 +6,7 @@ function Contact() {
         <div className="container-fluid bg-info" style={{ minHeight: '100vh' }}>
             <div className="row">
                 <div className="col-md-6 offset-md-1">
-                    <h2 class="ContactMeHeader">Contact Me:</h2>
+                    <h2 class="ContactMeHeader">Contact Me</h2>
                     <form>
                         <div className="form-group">
                             <label htmlFor="name" >Name:</label>
@@ -20,7 +20,7 @@ function Contact() {
                             <label htmlFor="message" >Message:</label>
                             <textarea className="form-control" id="message" name="message" rows="5" placeholder="Enter your message" required ></textarea>
                         </div>
-                        <button type="submit" className="btn btn-dark text-white" >Submit</button>
+                        <button onclick="window.location.href='jdalfaro4@gmail.com'" type="submit" className="btn btn-dark text-white" >Submit</button>
                     </form>
                 </div>
             </div>
